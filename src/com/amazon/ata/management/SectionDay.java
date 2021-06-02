20ce4dd47895397dad67184ef878f67417c6514e
Matt McCarley <mdmccarley89@gmail.com>
2021-06-02 13:46:54 -0700

    Add initial files

---
+++ src/com/amazon/ata/management/SectionDay.java
@@ -0,0 +1,8 @@
+package com.amazon.ata.generics.classroom.attendance.management;
+
+/**
+ * SectionDay enumerates the possible classroom days in ATA.
+ */
+public enum SectionDay {
+    TUESDAY, THURSDAY;
+}
